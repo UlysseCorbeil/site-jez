1823aa86b9facea8bec55b7986cd3fb77fb9b894
Ulysse <ulysse98@hotmail.com>
2020-03-09 15:19:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components';
+import image from './images/en_construction.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Reset from './styles/reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${Reset}
+`;
+
+const AppContainer = styled.section`
+    width: 100%;
+`;
+
+const Content = styled.section`
+    width: 100%;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ImageConstruction = styled.img`
+    max-width: 100%;
+    max-height: 100vh;
+    margin: 0;
+`;
+
+const Text = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin: 0 auto;
+    font-size: 1rem;
+`;
+
+class App extends Component {
+
+  render () {
+    return (
+      <AppContainer>
+
+        <GlobalStyle />
+
+        <Content>
+          <ImageConstruction src={image}/>
+        </Content>
+
+      </AppContainer>  
+    );
+    
+  }
 }
 
 export default App;
