694662537a6f28eda2b0aa11a24c37a52e22a1bd
UlysseCorbeil <ulysse98@hotmail.com>
2021-01-31 14:38:28 -0500

    images test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,97 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import image from '../images/en_construction.png';
 
 import styled from 'styled-components';
 
-const ImageConstruction = styled.img`
-    max-width: 100%;
-    max-height: 100vh;
-    margin: 0;
+import Project from "./Project";
+import water from "../images/24.jpg";
+import water2 from "../images/21.jpg";
+import water3 from "../images/9.jpg";
+
+const Image = styled.img`
 `;
 
 const Content = styled.section`
-    width: 100%;
-    display: flex;
-    flex-flow: column wrap;
-    justify-content: center;
-    align-items: center;
-`;
-
-const TextContainer = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-`;
-
-const TextInnerContainer = styled.div`
-    display: flex;
-    width: 100%;
-`;
-
-const Text = styled.div`
-    width: 30%;
-    font-size: calc(25px + 0.5vw);
-    margin-right: 2rem;
-    margin-left: 2rem;
-    margin-top: 2rem;
+  margin-left: 5%;
+  margin-right: 5%;
+  margin-top: 5%;
+  margin-bottom: 5%;
 `;
 
 class Home extends Component {
 
   render () {
-
-    let allTexts = [];
-    for(let i = 0; i < 4; i++) {
-      allTexts.push(
-        <Text style={{marginTop:  (4 * i) + "rem"}}> Site en construction </Text>
-      );
-    }
-
     return (
       <Content>
-        <ImageConstruction src={image} />
-        
-        <TextContainer>
-          <TextInnerContainer>
-            { allTexts.map((val, index) => (
-
-              <div>{ val }</div>
-    
-            )) }
-          </TextInnerContainer>
-        </TextContainer>
-
-        <TextContainer>
-          <TextInnerContainer>
-            { allTexts.map((val, index) => (
-
-              <div style={{marginTop: "10rem", marginLeft: "5rem"}}>{ val }</div>
-    
-            )) }
-          </TextInnerContainer>
-        </TextContainer>
-  
-        <TextContainer>
-          <TextInnerContainer>
-            { allTexts.map((val, index) => (
-
-              <div style={{marginTop: "20rem", marginLeft: "1rem"}}>{ val }</div>
-    
-            )) }
-          </TextInnerContainer>
-        </TextContainer>
-
-         <TextContainer>
-          <TextInnerContainer>
-            { allTexts.map((val, index) => (
-
-              <div style={{marginTop: "30rem", marginLeft: "5rem"}}>{ val }</div>
-    
-            )) }
-          </TextInnerContainer>
-        </TextContainer>
-
+        <Project 
+          images={ [water, water2, water3] } 
+          description={
+            [
+              "Material : glass, 383g of holy water, light ", 
+              "This is the glass shape of five years of my life, resting on the blessed liquid weight of five years of my life. ",
+              "The anxiety induced by the unstoppable passage of time has always been a burden that I couldn’t understand how most people seemed to be able to effortlessly live with. To help me cope with it, I started factually recording every day of my life in 2012. Making my time physical, something that could be touched, that had a texture, a weight, made more sense to me. Eventually, I became even more obsessed with the physical attributes of the book itself than with the memories it contained. ",
+              "I made a glass cast of its shape and obtained the consecration of its exact weight, as water. The water received the benediction of the priest of my favorite Cathedral only after I confessed my complete lack of Christian modesty. He told me I shouldn't pursue modesty, and blessed us, my water and me."
+            ]} 
+        />
 
         {/*<Link to={{
         pathname:'/' + 'project'
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+  width: 40%;
+  margin-top: 1%;
+`;
+
+const Description = styled.span`
+  width: 40%;
+  margin-top: 1%;
+`;
+
+const Content = styled.div`
+      width: 100%;
+      display: flex;
+      flex-flow: column wrap;
+      justify-content: center;
+      align-items: center;
+`;
 
 class Project extends Component {
 
   render () {
+
     return (
-      <div> project 1</div>
+      <Content>
+        {
+          this.props.images.map(image => (
+            <Image src={image}/>
+          ))
+        }
+
+        {
+          this.props.description.map(description => (
+            <Description>{description}</Description>
+          ))
+        }
+  
+      </Content>
     );
   }
 
