c1e66a2ec392c7ebd554c86ac1cec572915c7b96
UlysseCorbeil <ulysse98@hotmail.com>
2021-01-31 18:08:06 -0500

    s

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,7 +67,7 @@ class Home extends Component {
           images={ [water2, water, water3] } 
           description={
             [
-              "Material : glass, 383g of holy water, light ", 
+              "Materials : glass, 383g of holy water, light ", 
               "This is the glass shape of five years of my life, resting on the blessed liquid weight of five years of my life. ",
               "The anxiety induced by the unstoppable passage of time has always been a burden that I couldn’t understand how most people seemed to be able to effortlessly live with. To help me cope with it, I started factually recording every day of my life in 2012. Making my time physical, something that could be touched, that had a texture, a weight, made more sense to me. Eventually, I became even more obsessed with the physical attributes of the book itself than with the memories it contained. ",
               "I made a glass cast of its shape and obtained the consecration of its exact weight, as water. The water received the benediction of the priest of my favorite Cathedral only after I confessed my complete lack of Christian modesty. He told me I shouldn't pursue modesty, and blessed us, my water and me."
